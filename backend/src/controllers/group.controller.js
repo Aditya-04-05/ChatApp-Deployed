73006f267e93b8b9d9acd61ef5d160163ec1e3d3
Aditya Garg <gargaditya59@gmail.com>
2025-02-04 20:32:38 +0530

    added addMembers in groups

--- backend/src/controllers/group.controller.js
+++ backend/src/controllers/group.controller.js
@@ -37,7 +37,32 @@ export const createGroup = async (req, res) => {
   }
 };
 
-export const addMembers = async (req, res) => {};
+export const addMembers = async (req, res) => {
+  try {
+    const { newMembers } = req.body;
+    const { groupId } = req.params;
+    // const grpId = req.group._id;
+
+    if (!Array.isArray(newMembers) || newMembers.length == 0) {
+      return res.status(400).json({ messsage: "Members cann't be empty" });
+    }
+
+    const updatedGroup = await Group.findByIdAndUpdate(
+      groupId,
+      { $push: { groupMembers: { $each: newMembers } } },
+      { new: true }
+    );
+
+    if (!updatedGroup) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+
+    res.status(200).json(updatedGroup);
+  } catch (error) {
+    console.log("Error in Add Members controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 export const removeMembers = async (req, res) => {};
 
--- backend/src/routes/group.route.js
+++ backend/src/routes/group.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { createGroup } from "../controllers/group.controller.js";
+import { addMembers, createGroup } from "../controllers/group.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createGroup);
+router.put("/:groupId/add", addMembers);
 
 export default router;
