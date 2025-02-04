db852eb987f2a1960d5d64a0f8c2ee0bf5d575a6
Aditya Garg <gargaditya59@gmail.com>
2025-02-04 23:46:19 +0530

    added removeMembers and updateGroup and updated routes accordingly

--- backend/src/routes/group.route.js
+++ backend/src/routes/group.route.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { addMembers, createGroup } from "../controllers/group.controller.js";
+import {
+  addMembers,
+  createGroup,
+  removeMembers,
+  updateGroup,
+} from "../controllers/group.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createGroup);
 router.put("/:groupId/add", addMembers);
+router.put("/:groupId/remove", removeMembers);
+router.put("/:groupId/update", updateGroup);
 
 export default router;
